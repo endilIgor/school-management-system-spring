0fbd5bb5fe95e24ffaa05c304e5d1855cb9a5be0
Nagiitoshi <kitsunetech6@gmail.com>
2025-09-25 10:26:20 -0300

    first commit

---
+++ src/main/java/com/nagi/school_management_system_spring/SchoolManagementSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nagi.school_management_system_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemSpringApplication.class, args);
+	}
+
+}
